8c8724185d4c1f1ffe6dcf45385d7fe807d767cf
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-09-02 16:13:16 +0200

    Adds make:mixin command

---
+++ src/commands/make/mixin.js
@@ -0,0 +1,57 @@
+var commander = require('commander');
+var log = require('./../../../lib/logging');
+var generator = require('./../../../lib/generator');
+var _ = require('lodash');
+
+commander
+  .command('make:mixin [name]')
+  .description('scaffold a new mixin')
+  .action(function (name, options) {
+    program.action(name, options);
+  })
+  .on('--help', function () {
+    program.help();
+  });
+
+var program = {
+  action: function (name) {
+    if (!this.isValid(name)) {
+      process.exit(1);
+    }
+
+    _.mergeWith(generator.config, {
+      type: 'mixin',
+      templateDirectory: 'mixin',
+      output: {
+        directory: 'src/app/mixins/',
+      },
+      name: name,
+      isSplittable: false,
+    }, function (objValue, srcValue) {
+      if (_.isArray(objValue)) {
+        return objValue.concat(srcValue);
+      }
+    });
+
+    generator.run();
+  },
+  help: function () {
+    log('  Examples:');
+    log();
+    log('    # will scaffold a new mixin', 'muted');
+    log('    $ blucify make:mixin slot');
+    log('    # will scaffold a new mixin in a custom directory', 'muted');
+    log('    $ blucify make:mixin slot/main');
+    log();
+  },
+  isValid: function (name) {
+    var isValid = true;
+
+    if (!name) {
+      log('No name specified!', 'error');
+      isValid = false;
+    }
+
+    return isValid;
+  }
+};
---
+++ src/templates/mixin/template.js
@@ -0,0 +1,12 @@
+/* ============
+ * {{ name }} Mixin
+ * ============
+ *
+ * todo: add documentation here!
+ */
+
+export default {
+  mounted() {
+    console.log('{{ name }} mixin has been mounted!');
+  },
+};
